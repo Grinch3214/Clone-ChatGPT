d42dba014bdd960a03f59db77186dd92f21591a1
Max <scorpiomax90@gmail.com>
2024-04-09 22:58:18 +0300

    fixed types

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -13,7 +13,7 @@
 		</button>
 
 		<button
-		  @click="chatGptStore.openNeewChat"
+		  @click="openNewChat"
 		  class="mb-10 flex w-full gap-2 items-center text-sm hover:bg-secondary px-2 py-1 rounded-md transition-colors outline-none focus:shadow-border focus:shadow-border"
 		>
 			<Icon name="simple-icons:openai" size="1.5rem" />
@@ -33,7 +33,7 @@
 
 		<div class="overflow-x-hidden overflow-y-auto h-[calc(100vh-190px)] relative before:content-block before:absolute before:right-0 before:top-0 before:z-10 before:bottom-0 before:w-8 before:bg-gradient-to-l before:from-sidebar before:pointer-events-none">
 			<AppList
-			  v-for="(list, index) in reverseDatamessage"
+			  v-for="(list, index) in chatGptStore.dataMessages"
 			  :key="index"
 			  :prompt="list"
 			/>
@@ -56,21 +56,24 @@
 </template>
 
 <script setup lang="ts">
-	import { useChatGptStore } from '~/store/index'
+	import { GptAction, useChatGptStore } from '~/store/index'
 
 	const chatGptStore = useChatGptStore()
 
 	const elementToDetectOutsideClick = ref(null)
 	const modal = ref(false)
 
-	const deleteAllMessages = () => {
-		chatGptStore.deleteAlldataMessages()
-		modal.value = false
+	const openNewChat = () => {
+		chatGptStore.chatTree = []
+		chatGptStore.toggleEvent = false
 	}
 
-	const reverseDatamessage = computed(() => {
-		return chatGptStore.dataMessages
-	})
+	const deleteAllMessages = () => {
+		chatGptStore.dataMessages = []
+		chatGptStore.chatTree = []
+		chatGptStore[GptAction.SAVE_TO_LOCAL_STORAGE]()
+		modal.value = false
+	}
 
 	const handleClickOutside = (event) => {
 		clickOutsideHandler(event, elementToDetectOutsideClick.value, () => {
